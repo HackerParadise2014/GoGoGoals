fc16cf9842e9b1743a9b7e17994da528efa17cc1
Max Wofford <max.speed.wofford@gmail.com>
2014-09-24 13:05:07 -0600

    Name datatype is Text

--- gogogoals.rb
+++ gogogoals.rb
@@ -1,18 +1,22 @@
 require 'sinatra'
 require 'data_mapper'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
 
+# Routes
 get '/' do
   erb :index
 end
 
+# Models
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
 class Goal
   include DataMapper::Resource
   property :id, Serial
-  property :name, String, :required => true
+  property :name, Text, :required => true
   property :description, String
   property :worked_on, Boolean
   property :date_created, Date
-  DataMapper.finalize
 end
+ 
+ DataMapper.finalize
