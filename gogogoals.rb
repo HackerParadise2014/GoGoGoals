19023573c002022aee627ab2c733296f65eeb38e
WeiWei <wei@thiswei.com>
2014-09-24 12:36:30 -0600

    goal model

--- gogogoals.rb
+++ gogogoals.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
 
 get '/' do
   erb :index
+end
+
+class Goal
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String, :required => true
+  property :description, String
+  property :worked_on, Boolean
+  property :date_created, Date
+  DataMapper.finalize
 end
