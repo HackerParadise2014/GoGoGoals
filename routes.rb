798a0321c95bb0a550ec8e5e6f8281cb6b356b0c
Max Wofford <max.speed.wofford@gmail.com>
2014-10-02 14:00:46 -0600

    users can see individual goal pages

--- routes.rb
+++ routes.rb
@@ -5,9 +5,29 @@ get '/' do
   haml :index
 end
 
+get '/goals' do
+  redirect to('/')
+end
+
 post '/goals' do
   @data = params[:goal]
   @data["dm_user_id"] = current_user.id
   Goal.create(@data)
   redirect to('/')
+end
+
+post 'goals/:id' do
+  params[:id]
+  redirect to('/')
+end
+
+get '/goals/:id' do
+  login_required
+  if current_user.id == Goal.get(params[:id]).dm_user_id
+    @goals = Goal.all
+    @specific_goal = Goal.get(params[:id])
+    haml :goal
+  else
+    redirect to('/')
+  end
 end
